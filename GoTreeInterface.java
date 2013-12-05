2da83cf5d086aeb698cfbe8f2c5ea8495f335e4f
Simon C Chase <14scc2@brahorn.cs.williams.edu>
2013-12-05 15:42:28 -0500

    finished writing GoTree

--- GoTreeInterface.java
+++ GoTreeInterface.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public interface GoTreeInterface {
-    public void buildTree (int inputNodeNum, int hiddenNodeNum, int outputNodeNum, double mew);
+    public void buildTree (int inputNodeNum, int hiddenNodeNum, double mew);
     public void buildTree (File inFile);
-    public void train (int[] input, int outcome);
+    public void train (int[] input, int outcome) throws Exception;
     public double genMove (int[] input);
     public void toFile (File outFile);
 }
