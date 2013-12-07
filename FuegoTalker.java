0b8d1585afd1905231a9cbdb0b619fb3e5cd81ad
Simon C Chase <14scc2@tropicana.cs.williams.edu>
2013-12-07 15:07:45 -0500

    wrote push method for fuegotalker

--- FuegoTalker.java
+++ FuegoTalker.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
-import java.io.FileReader;
-import java.io.BufferedReader;
+import java.io.*;
 import java.util.*;
 
 /**
@@ -17,22 +16,48 @@ public class FuegoTalker {
     public FuegoTalker() {
 
     }
+    
+    /*
+     * push takes in a file representing the directory of all the sgf files we want to transform to our format
+     *
+     * it pushes out output to fuego to make all of the moves for the game represented by each file
+     */
 
-    public static void main(String[] args) throws IOException {		
-	String file_name = args[0];	
-	Transcripter writer = new Transcripter();
+    public static void push(File sgfPath) {
+	// get the array of all the files we want to transform in the directory
+	File[] sgfFileArray = sgfPath.listFiles();
+	Transcripter myTranscriptor = new Transcripter();
+	
+	for (int i = 0; i < sgfFileArray.length; i++) {
+	    // test to make sure that the file is actually there
+	    File curFile = sgfFileArray[i];
 
-	try {
-	    ReadFile file = new ReadFile(file_name);
-	    String contents = file.openFile();
-	    String board_state = writer.readShowBoard(contents);
-	    ArrayList<String> pro_moves = writer.readMoves(contents);
-	    writer.writeToFile("test_output.txt", board_state);
-	    writer.writeToFile("test_output.txt", pro_moves.get(0));
+	    if (curFile.isFile() && curFile.canRead()) {
+		// initialize fuego to a 9x9 board
+		System.out.println("boardsize 9");
+		ArrayList<String> movesList;
+
+		try {
+		    movesList = myTranscriptor.readMoves(curFile);
+		    for (String move : movesList) {
+			// for each move, we map to the move we output for fuego
+			System.out.println(myTranscriptor.genFuegoMove(move));
+		    } 		    
+		} catch (IOException e) {
+		    System.out.println(e);
+		}
+	    }
 	}
-	catch (IOException e) {
-	    System.out.println( e.getMessage() );
-	}	
+    }
+    
+    public static void pull() {
+	
+    }
+    
+    public static void main(String[] args) throws IOException {		
+	// read in a directory
+	File trainDir = new File(args[0]);
+	push(trainDir);
     }
 }
 
